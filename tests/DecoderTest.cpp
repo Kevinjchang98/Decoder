c8ddfd7c5eefcf5f2ca2558f37d1d7680a63dc7d
Kevin Chang <kevinjchang98@gmail.com>
2023-03-20 23:04:07 -0700

    remove quick brown fox test cases; reformat SpecialCharacters

--- tests/DecoderTest.cpp
+++ tests/DecoderTest.cpp
@@ -11,26 +11,8 @@ TEST(DecoderTest, Numbers) {
 }
 
 TEST(DecoderTest, SpecialCharacters) {
-  EXPECT_EQ(" !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~", Decoder::decryptCaesarShift(" !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"));
-}
-
-TEST(DecoderTest, ShiftOneNoSpacesLowercase) {
-  EXPECT_EQ("uifrvjdlcspxogpykvnqtpwfsuifmbazeph",
-            Decoder::decryptCaesarShift("thequickbrownfoxjumpsoverthelazydog"));
-}
-
-TEST(DecoderTest, ShiftOneNoSpacesUppercase) {
-  EXPECT_EQ("UIFRVJDLCSPXOGPYKVNQTPWFSUIFMBAZEPH",
-            Decoder::decryptCaesarShift("THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG"));
-}
-TEST(DecoderTest, ShiftOneSpacesLowercase) {
-  EXPECT_EQ("uif rvjdl cspxo gpy kvnqt pwfs uif mbaz eph",
-            Decoder::decryptCaesarShift("the quick brown fox jumps over the lazy dog"));
-}
-
-TEST(DecoderTest, ShiftOneSpacesUppercase) {
-  EXPECT_EQ("UIF RVJDL CSPXO GPY KVNQT PWFS UIF MBAZ EPH",
-            Decoder::decryptCaesarShift("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"));
+  EXPECT_EQ(" !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
+            Decoder::decryptCaesarShift(" !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"));
 }
 
 TEST(DecoderTest, ShiftFiveSentenceUppercase) {
